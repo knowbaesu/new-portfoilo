ae35c744d77878adb88c74959fd8d7328b6aec85
pjs <vkrwltn12@gmail.com>
2022-12-20 23:01:18 +0900

    intro

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -3,6 +3,21 @@ import "./intro.scss"
 export default function Intro() {
   return (
     <div className="intro" id="intro">
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/man.png" alt="" />
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>hi there</h2>
+          <h1>Park ji soo</h1>
+          <h3>Freelance<span></span></h3>
+        </div>
+        <a href="#portfolio">
+          <img src="assets/down.png" alt="" />
+        </a>
+      </div>
     </div>
   )
 }
