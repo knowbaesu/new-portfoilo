fc61be14ac308e3669a6f2ba78f4677b7a9868bf
pjs <vkrwltn12@gmail.com>
2023-01-13 23:49:21 +0900

    skeleton

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -8,7 +8,15 @@ export default function Portfolio() {
         <li>Featured</li>
         <li>Web App</li>
         <li>Mobile App</li>
+        <li>Desing</li>
+        <li>Branding</li>
       </ul>
+      <div className="container">
+        <div className="item">
+          <img src="" alt="" />
+          <h3></h3>
+        </div>
+      </div>
           </div>
   )
 }
