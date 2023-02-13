c6c887110d7f2399ad6f72da54ea901cdda9cbc4
pjs <vkrwltn12@gmail.com>
2023-02-13 23:41:41 +0900

    onClick

--- src/components/portfolioList/PortfolioList.jsx
+++ src/components/portfolioList/PortfolioList.jsx
@@ -3,7 +3,7 @@ import React from './portfoliolist.scss';
 export default function PortfolioList({title,active,setSelected}){
     return (
         <div>
-            <li className={active ? "portfolioList active" : "portfolioList"}>
+            <li className={active ? "portfolioList active" : "portfolioList" onclick={}}>
             {title}
             </li>
         </div>
